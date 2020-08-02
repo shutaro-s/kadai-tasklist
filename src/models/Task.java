0a003c98184277c60280e9a920e1ddf83281b2cf
shutaro.shibata <hwiirmapgeir3@gmail.com>
2020-08-02 23:00:58 +0900

    Add NewServlet.java

--- src/models/Task.java
+++ src/models/Task.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
             query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
             )
 })
-@Table(name = "tasklists")
+@Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
